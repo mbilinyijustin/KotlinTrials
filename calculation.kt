7e2c779eb94184215ab9449ef27cca07f3e4af92
mbilinyijustin <mbilinyijustin14@gmail.com>
2024-11-30 11:31:25 +0300

    Added a new file: Calculation.kt

---
+++ calculation.kt
@@ -0,0 +1,22 @@
+fun main(){
+    val number = 10
+    if (number > 0){
+        println("$number is positive.")
+    }
+    else{
+        println("$number is Negative.")
+    }
+    val marks = 85
+    if (marks >= 90){
+        println("Grade: A+")
+    }
+    else if (marks >= 80){
+        println("Grade: A")
+    }
+    else if (marks >= 70){
+        println("Grade: B")
+    }
+    else {
+        println("Grade: C")
+    }
+}
